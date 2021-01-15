3c350f02d86a3195fd3324c8d524648af4cd791f
charlotte <cmoche123@gmail.com>
2021-01-14 23:19:39 -0500

    edits .json.jb files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-# movie = Movie.create({title: "Kramer vs. Kramer", year: 1979, plot: "Joanna Kramer (Meryl Streep) leaves her husband and son. When she returns to claim custody of her son, a court battle ensues."})
