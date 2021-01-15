4ddd081c2da3f4fec5bff8a259e898130297d89c
charlotte <cmoche123@gmail.com>
2021-01-14 21:10:02 -0500

    adds movies to movie-app

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,2 +1,13 @@
 class Api::MoviesController < ApplicationController
+
+  def single_movie_action
+    @movie = Movie.first
+    render "movie.json.jb"
+  end
+
+  def all_movies_action
+    @movies = Movie.all
+    render "all_movies.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   
   namespace :api do
     get "/single_actor_path" => "actors#single_actor"
+    get "/single_movie_path" => "movies#single_movie_action"
+    get "/all_movies_path" => "movies#all_movies_action"
   end
 end
