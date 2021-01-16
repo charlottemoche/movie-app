19c4bda2252898f683ee5bac090d08182ea31ab9
charlotte <cmoche123@gmail.com>
2021-01-15 23:19:58 -0500

    adds paths to movie-app

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,8 +1,14 @@
 class Api::ActorsController < ApplicationController
 
-  def single_actor
-    @actor = Actor.find_by(id: 6)
-    render "single_actor.json.jb"
+  def first_actor
+    @actor = Actor.first
+    render "first_actor.json.jb"
+  end
+
+  def any_actor_action
+    actor_id = params[:id]
+    @actor = Actor.find_by(id: actor_id)
+    render "first_actor.json.jb"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   
   namespace :api do
-    get "/single_actor_path" => "actors#single_actor"
+    get "/first_actor_path" => "actors#first_actor"
     get "/single_movie_path" => "movies#single_movie_action"
     get "/all_movies_path" => "movies#all_movies_action"
+
+    get "/any_actor_path" => "actors#any_actor_action"
+    get "/actor_params_path/:id" => "actors#any_actor_action"
   end
-  
+
 end
