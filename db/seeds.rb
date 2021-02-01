5aa39e6646d0ca6306ef7c99209617662e8b4bd2
charlotte <cmoche123@gmail.com>
2021-01-31 19:40:38 -0500

    adds tonight's homework

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-# Actor.create([
-#   {first_name: "Cher", last_name: "Sarkisian", known_for: "Moonstruck", age: 74, gender: "Female"},
-#   {first_name: "Meryl", last_name: "Streep", known_for: "Kramer vs. Kramer", age: 71, gender: "Female"},
-#   {first_name: "Viola", last_name: "Davis", known_for: "Fences", age: 55, gender: "Female"},
-#   {first_name: "Christian", last_name: "Bale", known_for: "American Psycho", age: 46, gender: "Male"},
-#   {first_name: "Richard", last_name: "Gere", known_for: "Primal Fear", age: 71, gender: "Male"}
-# ])
+Actor.create([
+  {first_name: "Cher", last_name: "Sarkisian", known_for: "Moonstruck", age: 74, gender: "Female"},
+  {first_name: "Meryl", last_name: "Streep", known_for: "Kramer vs. Kramer", age: 71, gender: "Female"},
+  {first_name: "Viola", last_name: "Davis", known_for: "Fences", age: 55, gender: "Female"},
+  {first_name: "Christian", last_name: "Bale", known_for: "American Psycho", age: 46, gender: "Male"},
+  {first_name: "Richard", last_name: "Gere", known_for: "Primal Fear", age: 71, gender: "Male"}
+])
 
-# Movie.create([
-#   {title: "Moonstruck", year: "1987", plot: "Loretta Castorini (Cher) is set to marry Johnny, but she falls in love with his brother.", director: "Norman Jewison", english: true},
-#   {title: "Kramer vs. Kramer", year: "1979", plot: "Joanna Kramer (Meryl Streep) leaves her husband and son. When she returns to claim custody of her son, a court battle ensues.", director: "Robert Benton", english: true},
-#   {title: "Fences", year: "2016", plot: "Troy Maxson (Denzel Washington) creates tension within his family when his dreams don't come to fruition.", director: "Denzel Washington", english: true},
-#   {title: "American Psycho", year: "2000", plot: "Patrick Batement (Christian Bale) is a business man who is revealed to be a sociopathic serial killer.", director: "Mary Harron", english: true},
-#   {title: "Primal Fear", year: "1996", plot: "An attorney defends an altar boy accused of murdering the archbishop of Chicago", director: "Gregory Hoblit", english: true}
-# ])
+Movie.create([
+  {title: "Moonstruck", year: "1987", plot: "Loretta Castorini (Cher) is set to marry Johnny, but she falls in love with his brother.", director: "Norman Jewison", english: true},
+  {title: "Kramer vs. Kramer", year: "1979", plot: "Joanna Kramer (Meryl Streep) leaves her husband and son. When she returns to claim custody of her son, a court battle ensues.", director: "Robert Benton", english: true},
+  {title: "Fences", year: "2016", plot: "Troy Maxson (Denzel Washington) creates tension within his family when his dreams don't come to fruition.", director: "Denzel Washington", english: true},
+  {title: "American Psycho", year: "2000", plot: "Patrick Batement (Christian Bale) is a business man who is revealed to be a sociopathic serial killer.", director: "Mary Harron", english: true},
+  {title: "Primal Fear", year: "1996", plot: "An attorney defends an altar boy accused of murdering the archbishop of Chicago", director: "Gregory Hoblit", english: true}
+])
